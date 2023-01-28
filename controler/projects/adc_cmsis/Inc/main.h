9293152b46597bb6ac2ac8ade47a4e29f9800c5b
4i4urin <sanekshishurin@gmail.com>
2023-01-28 22:19:15 +0300

    crate project for adc

---
+++ controler/projects/adc_cmsis/Inc/main.h
@@ -0,0 +1,23 @@
+/*
+ * main.h
+ *
+ *  Created on: Jan 28, 2023
+ *      Author: shishel
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdint.h>
+#include "stm32f1xx.h"
+
+#define u8 		uint8_t
+#define u16 	uint16_t
+#define u32		uint32_t
+
+#define s8 		int8_t
+#define s16		int16_t
+#define s32		int32_t
+
+
+#endif /* MAIN_H_ */
