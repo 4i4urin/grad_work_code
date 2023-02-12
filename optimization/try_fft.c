d9f60a06454c1d72b77fdfef4eaa996589f46980
Shishel <sanekshishurin@gmail.com>
2023-02-13 01:59:40 +0300

    БПФ написано без оптимизации и с багами

--- optimization/try_fft.c
+++ optimization/try_fft.c
@@ -1,24 +1,84 @@
 #include "try_fft.h"
 
+#include "fft_table.h"
 
 int main(void)
 {
 	
-	u16 adc_vals[MEAS_NUM] = { 0 };
+	complex adc_vals[MEAS_NUM] = { 0 };
+	complex scratch[MEAS_NUM]  = { 0 };
+	s32 abs_arr[MEAS_NUM] = { 0 };
 
 	if ( read_arr_file(adc_vals, MEAS_NUM, "spi.txt") == NULL )
 		exit_code(E_ERR_FILE_READ, "ERROR: file read");
 
-	show_array(adc_vals, MEAS_NUM);
+	//show_array(adc_vals, MEAS_NUM);
+	//print_vector("Orig", adc_vals, MEAS_NUM);
+    fft( adc_vals, MEAS_NUM, scratch );
+    //print_vector(" FFT", adc_vals, MEAS_NUM);
+    for (u16 i = 0; i < MEAS_NUM; i++)
+    	abs_arr[i] = abs_2(adc_vals[i].Re, adc_vals[i].Im);
+
+    // show_array(abs_arr, MEAS_NUM);
+    out_array_file(abs_arr, MEAS_NUM, "res_fft_c.txt");
 
 	return 0;
 }
 
-
-void show_binary(u16 num)
+s32 abs_2(s32 x, s32 y)
 {
-	for (s8 i = 15; i >= 0; i--)
-		printf(  "%c", ( (num >> i) & 0x01 ) ? '1': '0');
+	return sqrt(pow(y, 2) + pow(x, 2));
+}
+
+void fft( complex *v, int n, complex *tmp )
+{
+    if(n <= 1) 			/* otherwise, do nothing and return */
+        return;
+
+    int k, m;
+    complex z, w, *vo, *ve;
+
+    ve = tmp; vo = tmp + n / 2;
+
+    for(k = 0; k < n / 2; k++) 
+    {
+        ve[k] = v[2 * k];
+        vo[k] = v[2 * k + 1];
+    }
+    fft( ve, n/2, v );		/* FFT on even-indexed elements of v[] */
+    fft( vo, n/2, v );		/* FFT on odd-indexed elements of v[] */
+    for(m = 0; m < n / 2; m++) 
+    {
+    	w = fft_table(n, m, MEAS_NUM);
+    	if (w.Im == 0 && w.Re == 0)
+    		exit_code(E_ERR_FFT_DIM, "ERROR: FFT DIMENTION");
+
+        z.Re = (w.Re * vo[m].Re - w.Im * vo[m].Im) / 1000;	/* Re(w*vo[m]) */
+        z.Im = (w.Re * vo[m].Im + w.Im * vo[m].Re) / 1000;	/* Im(w*vo[m]) */
+        v[m].Re         = ve[m].Re + z.Re;
+        v[m].Im         = ve[m].Im + z.Im;
+        v[m + n / 2].Re = ve[m].Re - z.Re;
+        v[m + n / 2].Im = ve[m].Im - z.Im;
+        /* DEBUG
+        if (   v[m].Re >= 500000  || v[m + n / 2].Re >= 500000
+        	|| v[m].Re <= -500000 || v[m + n / 2].Re <= -500000)
+        	printf("v[m] = {%d, %d}\nv[m+n/2] = {%d, %d}\nn = %d\nm = %d\n",
+        			v[m].Re, v[m].Im, v[m + n / 2].Re, v[m + n / 2].Im, n, m);
+        */
+    }
+}
+
+/* Print a vector of complexes as ordered pairs. */
+static void print_vector( const char *title, complex *x, int n)
+{
+    int i;
+    printf("%s (dim = %d):\n", title, n);
+
+    for(i = 0; i < n; i++ ) 
+        printf(" %5d,\t%5d\n", x[i].Re, x[i].Im);
+
+    putchar('\n');
+    return;
 }
 
 
@@ -38,14 +98,14 @@ void exit_code(e_errors ex_code, const char* pmsg_to_usr)
 }
 
 
-void show_array(u16* parr, u16 arr_size)
+void show_array(s32* parr, u16 arr_size)
 {
 	for (u16 i = 0; i < arr_size; i++)
-		printf("%hu\n", parr[i]);
+		printf("%d\n", parr[i]);
 }
 
 
-u16* read_arr_file(u16* parr, u16 arr_size, char* pfile_name)
+complex* read_arr_file(complex* parr, u16 arr_size, char* pfile_name)
 {
 	FILE* f = fopen(pfile_name, "r");
 
@@ -53,11 +113,28 @@ u16* read_arr_file(u16* parr, u16 arr_size, char* pfile_name)
 		exit_code(E_ERR_FILE_OPEN, "ERROR: file open");
 
 	for (u16 i = 0; i < arr_size; i++)
-		if (fscanf(f, "%hu", &parr[i]) == EOF)
+	{
+		parr[i].Im = 0;
+		if (fscanf(f, "%d", &parr[i].Re) == EOF)
 			return NULL;
+	}
 
 	if ( fclose(f) )
 		exit_code(E_ERR_FILE_CLOSE, "ERROR: file close");
 
 	return parr;
+}
+
+void out_array_file(s32* parr, u16 arr_size, const char* pfile_name)
+{
+	FILE* f = fopen(pfile_name, "w");
+
+	if (f == NULL)
+		exit_code(E_ERR_FILE_OPEN, "ERROR: file open");
+
+	for (u16 i = 0; i < arr_size; i++)
+		fprintf(f, "%d\n", parr[i]);
+
+	if ( fclose(f) )
+		exit_code(E_ERR_FILE_CLOSE, "ERROR: file close");
 }
--- optimization/try_fft.h
+++ optimization/try_fft.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define u8 		uint8_t
 #define u16 	uint16_t
@@ -15,21 +16,34 @@
 #define s32  	int32_t
 #define s64 	int32_t
 
-#define MEAS_NUM 4096
+#define MEAS_NUM 	4096
+
+#define PI	3.14159265358979323846264338327950288
 
 typedef enum _e_errors
 {
 	E_ERR_FILE_OPEN = 1,
 	E_ERR_FILE_READ, 
 	E_ERR_FILE_WRITE, 
-	E_ERR_FILE_CLOSE
+	E_ERR_FILE_CLOSE,
+	E_ERR_FFT_DIM
 } e_errors;
 
+typedef struct
+{
+    s32 Re; 
+    s32 Im;
+} complex;
 
-void show_array(u16* parr, u16 arr_size);
-u16* read_arr_file(u16* parr, u16 arr_size, char* pfile_name);
+void show_array(s32* parr, u16 arr_size);
+void out_array_file(s32* parr, u16 arr_size, const char* pfile_name);
+complex* read_arr_file(complex* parr, u16 arr_size, char* pfile_name);
 void exit_code(e_errors ex_code, const char* pmsg_to_usr);
 u16 reverse(u16 byte);
-void show_binary(u16 num);
+
+// fft 
+static void print_vector( const char *title, complex *x, int n);
+void fft( complex *v, int n, complex *tmp );
+s32 abs_2(s32 x, s32 y);
 
 #endif /* TRY_FFT_H */
